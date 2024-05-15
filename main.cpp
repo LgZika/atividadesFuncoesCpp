686e3d1de3d8a2b00de523e491089b8f66743f17
Luiz Gustavo Dias Pulz <162385594+LgZika@users.noreply.github.com>
2024-05-15 00:37:49 +0000

    upadate

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
@@ -261,29 +262,241 @@ void inverteCaracteres(){
 
     cout << "Insira uma pequena palavra: ";
     cin >> palavra;
-     
+ 
     reverse(palavra.begin(), palavra.end()); // função reverse(), reverte a stream que é passada como parâmetro. 
 
     cout << "A palavra inserida foi invertida: " + palavra << endl;  
 }
 
+void substituiVogais(){
+    // Exercicio 15
+    // Declare um array de caracteres frase com 20 posições. 
+    // Leia uma frase do usuário usando cin e armazene-a em frase. 
+    // Substitua todas as vogais da frase por '*'. 
+    // Imprima a frase com as vogais substituídas usando cout.
+
+    string frase;
+    char vogais[] = {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'};
+    int len; 
+    int lenV = sizeof(vogais) / sizeof(vogais[0]); // mecanismo de sizeof para pegar o tamanho fo vetor
+
+    cout << "Insira uma pequena frase: ";
+    getline(cin, frase);
+    len = frase.size(); // funcao size para pegar o tamanho de uma String, sizeoff só funciona na string se seu tamanho estiver especificado ([])
+    
+    for(int i = 0; i < len; i++){
+        for(int j = 0; j < lenV; j++){
+            if(frase[i] == vogais[j]){
+                frase[i] = '*';
+            }
+        }
+    } 
+    cout << "A frase inserida foi modificada: " + frase << endl;
+}
+
+void imprimeNomeIdade(){
+    // Exercicio 16
+    // Declare um array de caracteres nomes com 10 posições e um array de inteiros idades com 10 posições. 
+    // Leia os nomes e idades de 10 pessoas usando cin e armazene-os nos arrays. 
+    // Imprima a lista de nomes e idades em colunas usando cout.
+
+    string nomes[10];
+    int idades[10];
+    int lenVetor = sizeof(nomes) / sizeof(nomes[0]);
+
+    cout << "Preencha os dados no formato 'Nome,Idade'" << endl;
+    for(int i = 0; i < lenVetor; i++){
+        cout << "ID " << i << ": ";
+        getline(cin, nomes[i], ',');
+        cin >> idades[i];
+        cin.ignore();
+    }
+
+    cout << endl;
+    cout << "Exibindo dados armazenados..." << endl;
+    for(int i = 0; i < lenVetor; i++){
+        cout << "ID " << i << " - " << nomes[i] << " - " << idades[i] << " anos" << endl;
+    }
+}
+
+void imprimeProdutoPreco(){
+    // Exercicio 17
+    // Declare um array de caracteres produtos com 20 posições e um array de floats precos com 20 posições. 
+    // Leia os nomes e preços de 10 (?) produtos usando cin e armazene-os nos arrays. 
+    // Imprima a lista de produtos e preços em colunas 
+
+    string produtos[5];
+    float valorProduto[5];
+    int lenVetor = sizeof(produtos) / sizeof(produtos[0]);
+
+    cout << "Preencha os dados no formato 'Produto,Preco'" << endl;
+    for(int i = 0; i < lenVetor; i++){
+        cout << "ID " << i << ": ";
+        getline(cin, produtos[i], ',');
+        cin >> valorProduto[i];
+        cin.ignore();
+    }
+
+    cout << endl;
+    cout << "Exibindo dados armazenados..." << endl;
+    for(int i = 0; i < lenVetor; i++){
+        cout << "ID " << i << " - " << produtos[i];
+        int tam = 20 - (produtos[i].size());
+        for(tam; tam > 0; tam--){
+            cout << " ";
+        } 
+        cout << "- R$ " << valorProduto[i] << endl;
+    }
+}
+
+void organizaAlfabeticamente(){
+    // Exercicio 18
+    // Declare um array de caracteres cidades com 15 posições. 
+    // Leia os nomes de 15 cidades do usuário usando cin e armazene-as no array. 
+    // Ordene as cidades em ordem alfabética e imprima a lista ordenada usando cout.
+
+    string cidades[15]; //= {"Sao Paulo", "Rio de Janeiro", "Brasilia", "Fortaleza", "Salvador", "Belo Horizonte", "Manaus","Curitiba", "Recife", "Goiania", "Porto Alegre", "Belem", "Guarulhos", "Campinas", "Sao Luis"};
+    string cidadesTemporaria = " ";
+    int lenVetor = sizeof(cidades) / sizeof(cidades[0]);
+
+    cout << "Entre com nomes de cidades: " << endl;
+    for(int i = 0; i < lenVetor; i++){
+        cout << "pos " << i << " ";
+        getline(cin, cidades[i]);
+        //cin.ignore();
+    }
+    
+    for(int i = 0; i < lenVetor; i++){
+        for(int j = 0; j < lenVetor; j++){
+            if(cidades[i].compare(cidades[j]) < 0){ //verifica se a proxima seção do vetor é menor em ordem alfabetica (A<B<C...<Z)
+                cidadesTemporaria = cidades[i];
+                cidades[i] = cidades[j];
+                cidades[j] = cidadesTemporaria;
+            }
+        }
+    }
+
+    cout << endl;
+    cout << "Exibindo cidades em ordem:" << endl;
+    for(int i = 0; i < lenVetor; i++){
+        cout << "pos " << i << " " << cidades[i] << endl;
+    }
+}
+
+void contaPalavras(){
+    // Exercicio 19
+    // Declare um array de caracteres texto com 50 posições. 
+    // Leia um texto do usuário usando cin e armazene-o em texto. 
+    // Conte o número de palavras no texto e imprima o resultado usando cout.
+
+    char texto[50];
+    int lenVetor = sizeof(texto) / sizeof(texto[0]);
+    int x = 0;
+
+    cout << "Entre com uma frase: ";
+    cin.getline(texto, 50);
+    cout << endl;
+
+    for(int i = 0; i < lenVetor; i++){
+        if(texto[i] == ' '){
+            x++;
+        }
+    }  
+    cout << "A frase: \"" << texto << "\" possui " << x << " palavras!" << endl;
+}
+
+void encontraMaiorPalavra(){
+    // Exercicio 20
+    // Declare um array de caracteres frase com 20 posições. 
+    // Leia uma frase do usuário usando cin e armazene-a em frase. 
+    // Encontre a palavra mais longa da frase e imprima a palavra usando cout.
+
+    char frase[20], palavra[20], maiorPalavra[20], menorPalavra[10];
+    int lenVetor = sizeof(frase) / sizeof(frase[0]);
+    int numeroPalavra = 0, tamanhoMaior = 0, tamanhoMenor = 20;
+    int i, j, tamanhoPalavra;
+
+    cout << "Insira uma pequena frase: ";
+    fgets(frase, 21, stdin);
+    
+    for(i = 0; frase[i] != '\0'; i++){
+        if((frase[i] == ' ') || ( frase[i+1] == '\0')){          // conta o numero de palavras da frase
+            numeroPalavra++;                                     // incrementa o numero de palavras na variavel numeroPalavra                
+        }
+    }
+    
+    for(i = 0; i < lenVetor; i++){                               // percorre a frase
+        j = 0;                                                   //reinicia a posicao do array da palavra a cada loop                   
+        while((frase[i] != ' ') && (frase[i+1] != '\0')){       // percorre a palavra atual
+            palavra[j] = frase[i];                              // atribui os caracteres de frase para a palavra enquanto nao achar um espaco ou o final    
+            i++;                                            
+            j++;
+        }
+
+        if(j > 0){                                          // evita que palavra receba o cacacter de final string na primeira posicao
+            palavra[j] = '\0';                              // a palavra atual recebe o finalizador de string
+        }
+        tamanhoPalavra = strlen(palavra);                   // pega o tamanho da palavra atual
+        
+        if(tamanhoPalavra > tamanhoMaior){                  // verifica se a palavra atual é maior que a maior anterior
+            tamanhoMaior = tamanhoPalavra;                  // se for maior recebe o tamanho da palavra atual a armazena o tamanho atual como maior
+            strcpy(maiorPalavra, palavra);                  // e aloca a palavra na variavel de maiorPalavra
+        } 
+        if(tamanhoPalavra < tamanhoMenor){                  // verifica se a palavra atual é menor que a menor anterior
+            tamanhoMenor = tamanhoPalavra;                  // se for menor recebe o tamanho da palavra atual a armazena o tamanho atual como menor                  
+            strcpy(menorPalavra, palavra);                  // e aloca a palavra na variavel de menorPalavra
+        }
+    }
+
+    cout << endl;
+    cout << "Frase Inserida: " << frase << endl;
+    cout << "-> possui " << numeroPalavra << " palavras;" << endl;
+    cout << "-> a MAIOR palavra da frase eh: \"" << maiorPalavra << "\" e possui " << tamanhoMaior << " letras;" << endl;
+    cout << "-> a MENOR palavra da frase eh: \"" << menorPalavra << "\" e possui " << tamanhoMenor << " letras;" << endl;
+    cout << endl;
+}
 
 int main(){
     // Exercícios sobre Strings e Arrays em C++
     imprimeNome();
+    cout << endl;
     imprimeVogal();
+    cout << endl;
     verificaVogal();
+    cout << endl;
     imprimePalavra();
+    cout << endl;
     imprimeFrase();
+    cout << endl;
     imprimePar();
+    cout << endl;
     imprimeFrutas();
+    cout << endl;
     imprimeCores();
+    cout << endl;
     imprimeAnimais();
+    cout << endl;
     imprimeDiaDaSemana();
+    cout << endl;
     comparaPalavras();
+    cout << endl;
     contaNumeroVogais();
+    cout << endl;
     encontraLetraA();
+    cout << endl;
     inverteCaracteres();
+    cout << endl;
+    substituiVogais();
+    cout << endl;
+    imprimeNomeIdade();
+    cout << endl;
+    organizaAlfabeticamente();
+    cout << endl;
+    contaPalavras();
+    cout << endl;
+    encontraMaiorPalavra();
+    cout << endl;
 
+    //
     return 0;
 }
